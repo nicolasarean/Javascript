13a4d9606f5d20f27e29de5f332d51db5846c655
nicolasarean <108200447+nicolasarean@users.noreply.github.com>
2022-09-03 13:06:50 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let entrada = prompt("Ingresar un nombre").toUpperCase();
+
+    while(entrada != "ESC"){
+         switch(entrada){
+            case "ANA":
+            alert("HOLA ANA");
+
+        break;
+        case "JUAN":
+            alert("HOLA JUAN");
+        
+        break;
+        default:
+            alert("QUIEN SOS?")
+        break; 
+    }
+    }
+    alert("Gracias");
